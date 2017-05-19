5c7e1112a57921449db460c45d856bfaa56c6497
Sarah Cattin <scattin000@hotmail.com>
2017-05-19 13:47:40 -0600

    added fiiles and contents that I have currently

---
+++ JavaScript/main.js
@@ -0,0 +1,72 @@
+///DOM Objects///
+var video = null;
+
+function checkUserMedia() {
+    return navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia || null;
+}
+
+/*Function displayVideo Only called when camera button is selected  */
+function displayVideo() {
+    if (checkUserMedia()) {
+        //turn off the video 
+        //var videoPlaying = false;
+        // set video standards 
+        var constraints = {
+            video: true,
+            audio: false
+        };
+        // set up the video to display 
+        var media = navigator.getUserMedia(constraints, function(stream) {
+            // URL Object is different in WebKit
+            var url = window.URL || window.webkitURL;
+
+            video = document.getElementById("cameraDisplay")
+
+            // create the url and set the source of the video element
+            video.src = url ? url.createObjectURL(stream) : stream
+
+            // Start the video
+            video.play()
+                //videoPlaying = true;
+                // third parameter (error handling)
+        }, function(error) {
+            console.log("ERROR")
+            console.log(error)
+        });
+    }
+} // end displayVideo
+
+/*Show Video Function 
+function showVideo() {
+    // transition rather than hide & show to remove odds of jarring 
+       // document.getElementById("cameraDisplay").style.height = "200px"
+    document.getElementById("cameraDisplay").style.height = "500px"
+}
+
+function hideVideo() {
+    // document.getElementById("cameraDisplay").style.height = "0px"
+    document.getElementById("cameraDisplay").style.height = "0px"
+}*/
+
+function displayPhoto() {
+    capturePhoto()
+    transitionDisplay()
+}
+
+function capturePhoto() {
+    //if (videoPlaying) {
+    var canvas = document.getElementById('canvasPhoto');
+    var context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, 1920, 2560, 0, 0, 480, 640); //480,640);
+}
+
+function transitionDisplay() {
+    // set the video to opaque & set the canvas to transparent 
+
+    //swap the video for the picture
+    //document.getElementById("cameraDisplay").style.opacity = 0
+    document.getElementById("canvasPhoto").style.opacity = 1
+}
